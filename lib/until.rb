2282ae6334dd9c01ab70b3a6e552c35037a2e1e8
Romena Murgor <romenamurgor@gmail.com>
2020-03-09 19:40:20 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,14 +1,14 @@
 def using_until 
  puts levitation_force = '<10n\n'
-	"Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
+	'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
+  'Wingardium Leviosa'
   
   
   
