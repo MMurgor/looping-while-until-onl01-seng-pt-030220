460fb20ea93b2b22cb3dd3f40ef2f16f253a7f53
Romena Murgor <romenamurgor@gmail.com>
2020-03-09 19:21:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,4 +1,4 @@
-def expect using_while (looping_string)
+def expect using_while looping_string
  puts levitation_force = "10/n"
 	"Wingardium Leviosa"
   "Wingardium Leviosa"
@@ -10,4 +10,4 @@ def expect using_while (looping_string)
   "Wingardium Leviosa"
   "Wingardium Leviosa"
   "Wingardium Leviosa"
-end
+end-of-input
