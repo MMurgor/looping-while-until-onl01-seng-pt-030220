cfcb00f43a140430d4eccbec6130699c610e8fea
Romena Murgor <romenamurgor@gmail.com>
2020-03-09 20:02:14 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,15 +1,5 @@
 def expect using_while 
- puts levitation_force = "10/n"
-	"Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
-  "Wingardium Leviosa"
+
 
 end
 
